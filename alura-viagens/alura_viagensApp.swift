354dd4cb40fd83bf0210e2da395f313b5a269b2d
Natalia Braz <naabraz@gmail.com>
2023-09-08 14:31:12 -0300

    Initial Commit

---
+++ alura-viagens/alura_viagensApp.swift
@@ -0,0 +1,17 @@
+//
+//  alura_viagensApp.swift
+//  alura-viagens
+//
+//  Created by Natalia Braz on 08/09/23.
+//
+
+import SwiftUI
+
+@main
+struct alura_viagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
