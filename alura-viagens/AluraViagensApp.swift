0c4522645e8a89ba6b567ea2406aaa4619987a13
Natalia Braz <naabraz@gmail.com>
2023-09-11 11:46:36 -0300

    feat: customize header

---
+++ alura-viagens/AluraViagensApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct AluraViagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- alura-viagens/ContentView.swift
+++ alura-viagens/ContentView.swift
@@ -1,26 +1,41 @@
-//
-//  ContentView.swift
-//  alura-viagens
-//
-//  Created by Natalia Braz on 08/09/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+  var body: some View {
+    GeometryReader { view in
+      VStack {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+          Text("alura viagens")
+            .foregroundColor(Color.white)
+            .font(.custom("Avenir Black", size: 20))
+            .padding(.top, 50)
+          Text("ESPECIAL")
+            .foregroundColor(Color.white)
+            .font(.custom("Avenir Book", size: 20))
+            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            .padding(.leading, 30)
+          Text("BRASIL")
+            .foregroundColor(Color.white)
+            .font(.custom("Avenir Black", size: 23))
+            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            .padding(.leading, 30)
         }
-        .padding()
+        .frame(width: view.size.width, height: 180, alignment: .top)
+        .background(Color.purple)
+        
+        List {
+          Text("Rio de Janeiro")
+          Text("Ceará")
+          Text("Atibaia")
+          Text("Pernambuco")
+        }
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
