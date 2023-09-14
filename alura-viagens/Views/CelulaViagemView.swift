c1d4080c6e38a13cd15205b6df5a3989c0f20dee
Natalia Braz <naabraz@gmail.com>
2023-09-14 10:48:14 -0300

    chore: adjust list to ipad

--- alura-viagens/Views/CelulaViagemView.swift
+++ alura-viagens/Views/CelulaViagemView.swift
@@ -3,17 +3,30 @@ import SwiftUI
 struct CelulaViagemView: View {
   var viagem: Viagem
   
+  @Environment(\.horizontalSizeClass) var horizontalSizeClass
+  
   var body: some View {
     VStack(alignment: .leading) {
       Text(viagem.titulo)
+        .font(.custom("Avenir",
+                      size: self.horizontalSizeClass == .compact ? 14 : 24))
+
       Image(viagem.imagem)
         .resizable()
-        .frame(height: 125)
-        
+        .aspectRatio(contentMode: .fill)
+        .frame(height: self.horizontalSizeClass == .compact ? 125 : 200)
+        .clipped()
+
       HStack {
         Text(viagem.quantidadeDeDias)
+          .font(.custom("Avenir",
+                        size: self.horizontalSizeClass == .compact ? 14 : 24))
+
         Spacer()
+
         Text(viagem.valor)
+          .font(.custom("Avenir",
+                        size: self.horizontalSizeClass == .compact ? 14 : 24))
       }
     }
   }
