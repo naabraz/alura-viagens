76fb1d99683482e34ca58529d2fe676644ee57dd
Natalia Braz <naabraz@gmail.com>
2023-09-14 10:19:40 -0300

    chore: adjust header size on ipad

--- alura-viagens/Views/ContentView.swift
+++ alura-viagens/Views/ContentView.swift
@@ -1,11 +1,15 @@
 import SwiftUI
 
 struct ContentView: View {
+  @Environment(\.horizontalSizeClass) var horizontalSizeClass
+
   var body: some View {
     GeometryReader { view in
       VStack {
         HeaderView()
-          .frame(width: view.size.width, height: 200, alignment: .top)
+          .frame(width: view.size.width,
+                 height: self.horizontalSizeClass == .compact ? 240 : 340,
+                 alignment: .top)
         List (viagens) { viagem in
           CelulaViagemView(viagem: viagem)
         }
--- alura-viagens/Views/HeaderView.swift
+++ alura-viagens/Views/HeaderView.swift
@@ -1,50 +1,59 @@
 import SwiftUI
 
 struct HeaderView: View {
+  @Environment(\.horizontalSizeClass) var horizontalSizeClass
+  
   var body: some View {
     GeometryReader { view in
       VStack {
         VStack {
           Text("alura viagens")
             .foregroundColor(Color.white)
-            .font(.custom("Avenir Black", size: 20))
+            .font(.custom("Avenir Black",
+                          size: self.horizontalSizeClass == .compact ? 20 : 30))
             .padding(.top, 50)
           Text("ESPECIAL")
             .foregroundColor(Color.white)
-            .font(.custom("Avenir Book", size: 20))
+            .font(.custom("Avenir Book",
+                          size: self.horizontalSizeClass == .compact ? 20 : 30))
             .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
             .padding(.leading, 30)
           Text("BRASIL")
             .foregroundColor(Color.white)
-            .font(.custom("Avenir Black", size: 23))
+            .font(.custom("Avenir Black",
+                          size: self.horizontalSizeClass == .compact ? 23 : 33))
             .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
             .padding(.leading, 30)
         }
-          .frame(width: view.size.width, height: 180, alignment: .top)
+        .frame(width: view.size.width,
+               height: self.horizontalSizeClass == .compact ? 200 : 310,
+               alignment: .top)
           .background(Color.purple)
           
         HStack {
           Button(action: {}) {
             Text("Hotéis")
-              .font(.custom("Avenir Medium", size: 17))
+              .font(.custom("Avenir Medium",
+                            size: self.horizontalSizeClass == .compact ? 17 : 24))
               .foregroundColor(.white)
           }
             .frame(width: 100, height: 50)
             .overlay(RoundedRectangle(cornerRadius: 10).stroke(.blue, lineWidth: 10))
             .background(.blue)
-            .offset(x: 50)
+            .offset(x: self.horizontalSizeClass == .compact ? 50 : view.size.width / 4)
             
           Spacer()
             
           Button(action: {}) {
             Text("Pacotes")
-              .font(.custom("Avenir Medium", size: 17))
+              .font(.custom("Avenir Medium",
+                            size: self.horizontalSizeClass == .compact ? 17 : 24))
               .foregroundColor(.white)
           }
             .frame(width: 100, height: 50)
             .overlay(RoundedRectangle(cornerRadius: 10).stroke(.orange, lineWidth: 10))
             .background(.orange)
-            .offset(x: -50)
+            .offset(x: self.horizontalSizeClass == .compact ? -50 : -view.size.width / 4)
         }
           .offset(y: -35)
       }
